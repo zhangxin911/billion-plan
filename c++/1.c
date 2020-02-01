075f4b6da7721c7d6b4e20f35ef1c305d9cdc8e6
HighFans <453790557@qq.com>
2020-02-01 14:51:22 +0800

    orm begin

---
+++ c++/1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main()
+{
+   /* 我的第一个 C 程序 */
+   // printf("Hello, World! \n");
+	
+   // putchar('H');
+   // putchar('\007');
+   // putchar('\n');
+   // putchar('\b');
+
+	
+
+   return 0;
+}
+
