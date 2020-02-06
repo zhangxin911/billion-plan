198f7857cf99c8ab2186e4c0d64eed26bb22048f
zhangxin911 <453790557@qq.com>
2020-02-06 16:27:28 +0800

    cart redux
    redux v1

--- vg/src/cart/cart.jsx
+++ vg/src/cart/cart.jsx
@@ -2,34 +2,96 @@ import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 
 import {createStore} from 'redux';
-let listData=require('mockData/listData.js');
+import './cart.less';
 
+let {listData,cartData}=require('mockData/listData.js');
+// let cartData=require('mockData/cartData.js');
  
-const reducer=function(state=listData,action){
+const reducer=function(state=cartData,action){
+	console.log(state,action);
+
+	switch (action.type){
+		case 'addCart':
+			(()=>{
+				state.cart[action.payload.index].own=action.payload.own;
+				return state;
+			})()
+			break;
+		default:break;
+	}
 	return state;
 }
 
 const store=createStore(reducer);
 
-console.log(store.getState())
+
 
 class Cart extends Component{
+
+	componentWillMount(){
+		let cartData=store.getState();
+		this.setState({
+			cartData
+		})
+	}
+
 	constructor(){
 		super();
+		this.add=this.add.bind(this);
+		store.subscribe(()=>{
+			this.setState({
+				cartData:store.getState()
+			})
+		});
+	}
+	add(val,index){
+		
+		const action={
+			type:'addCart',
+			payload:{
+				own:val,
+				index:index
+			}
+		}
+		store.dispatch(action)
 
 	}
 	render(){
+		let cartDom=[],{cart}=this.state.cartData;
+		console.log(cart)
+		cart.map((item,index)=>{
+			cartDom.push(
+				<div className="cart-item" key={index}>
+					<div className="cart-item-l">
+						<img className="cart-item-pic" src='#'></img>
+						<div className="cart-item-detail"> 
+							<div>{item.name}</div>
+							<div>{item.nowPrice*item.own}</div>
+						</div>
+					</div>
+					<div className="cart-item-r">
+						<div className="reduce">-</div>{item.own}<div className="add" onClick={()=>{this.add(++item.own,index)}}>+</div>
+					</div>
+				</div>
+			)
+		})
 		return (
 			<div className="cart">
-				<div className="cart-title">
+				<div className="cart-mask">
+					
+				</div>
+				<div className="cart-content">
+					<div className="cart-title">
 					已选商品 <span>清空</span>
+					</div>
+					<div className="cart-list">
+						{cartDom}
+					</div>
+					<div className="cart-discount">
+						
+					</div>
 				</div>
-				<div className="cart-list">
-					
-				</div>
-				<div className="cart-discount">
-					
-				</div>
+				
 			</div>
 		)
 	}
